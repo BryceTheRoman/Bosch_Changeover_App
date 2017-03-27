43af4656a4be271bf0151f7f58aa82d2aab93123
Eric Hofesmann <ehofesmann@gmail.com>
2017-03-26 23:23:42 -0400

    Add first version of buttons to lines

--- Bosch_Changeover_App/Bosch_Changeover_App/LineUserControl.Designer.cs
+++ Bosch_Changeover_App/Bosch_Changeover_App/LineUserControl.Designer.cs
@@ -28,110 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.line1ListBox = new System.Windows.Forms.ListBox();
             this.line1Label = new System.Windows.Forms.Label();
             this.line2Label = new System.Windows.Forms.Label();
             this.line2ListBox = new System.Windows.Forms.ListBox();
             this.line3Label = new System.Windows.Forms.Label();
             this.line3ListBox = new System.Windows.Forms.ListBox();
-            this.line1Part = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.panelOnLine1 = new System.Windows.Forms.Panel();
+            this.panelOffLine1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // line1ListBox
-            // 
-            this.line1ListBox.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.line1ListBox.FormattingEnabled = true;
-            this.line1ListBox.ItemHeight = 22;
-            this.line1ListBox.Items.AddRange(new object[] {
-            "553\t             83",
-            "513\t             10",
-            "932\t             43"});
-            this.line1ListBox.Location = new System.Drawing.Point(80, 92);
-            this.line1ListBox.Name = "line1ListBox";
-            this.line1ListBox.Size = new System.Drawing.Size(191, 400);
-            this.line1ListBox.TabIndex = 0;
             // 
             // line1Label
             // 
             this.line1Label.AutoSize = true;
-            this.line1Label.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.line1Label.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.line1Label.Location = new System.Drawing.Point(130, 29);
             this.line1Label.Name = "line1Label";
-            this.line1Label.Size = new System.Drawing.Size(89, 32);
+            this.line1Label.Size = new System.Drawing.Size(93, 31);
             this.line1Label.TabIndex = 1;
             this.line1Label.Text = "Line 1";
             // 
             // line2Label
             // 
             this.line2Label.AutoSize = true;
-            this.line2Label.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.line2Label.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.line2Label.Location = new System.Drawing.Point(357, 29);
             this.line2Label.Name = "line2Label";
-            this.line2Label.Size = new System.Drawing.Size(89, 32);
+            this.line2Label.Size = new System.Drawing.Size(93, 31);
             this.line2Label.TabIndex = 3;
             this.line2Label.Text = "Line 2";
             // 
             // line2ListBox
             // 
-            this.line2ListBox.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line2ListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.line2ListBox.FormattingEnabled = true;
-            this.line2ListBox.ItemHeight = 22;
+            this.line2ListBox.ItemHeight = 24;
             this.line2ListBox.Items.AddRange(new object[] {
             "553\t          0:57",
             "513\t          2:30",
             "932\t          4:00"});
             this.line2ListBox.Location = new System.Drawing.Point(304, 92);
             this.line2ListBox.Name = "line2ListBox";
-            this.line2ListBox.Size = new System.Drawing.Size(189, 400);
+            this.line2ListBox.Size = new System.Drawing.Size(189, 388);
             this.line2ListBox.TabIndex = 2;
             // 
             // line3Label
             // 
             this.line3Label.AutoSize = true;
-            this.line3Label.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line3Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.line3Label.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.line3Label.Location = new System.Drawing.Point(584, 29);
             this.line3Label.Name = "line3Label";
-            this.line3Label.Size = new System.Drawing.Size(89, 32);
+            this.line3Label.Size = new System.Drawing.Size(93, 31);
             this.line3Label.TabIndex = 5;
             this.line3Label.Text = "Line 3";
             // 
             // line3ListBox
             // 
-            this.line3ListBox.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.line3ListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.line3ListBox.FormattingEnabled = true;
-            this.line3ListBox.ItemHeight = 22;
+            this.line3ListBox.ItemHeight = 24;
             this.line3ListBox.Items.AddRange(new object[] {
             "553\t             83",
             "513\t             10",
             "932\t             43"});
             this.line3ListBox.Location = new System.Drawing.Point(534, 92);
             this.line3ListBox.Name = "line3ListBox";
-            this.line3ListBox.Size = new System.Drawing.Size(192, 400);
+            this.line3ListBox.Size = new System.Drawing.Size(192, 388);
             this.line3ListBox.TabIndex = 4;
-            // 
-            // line1Part
-            // 
-            this.line1Part.AutoSize = true;
-            this.line1Part.Location = new System.Drawing.Point(89, 73);
-            this.line1Part.Name = "line1Part";
-            this.line1Part.Size = new System.Drawing.Size(26, 13);
-            this.line1Part.TabIndex = 6;
-            this.line1Part.Text = "Part";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(206, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Number Left";
             // 
             // label1
             // 
@@ -151,20 +118,35 @@
             this.label3.TabIndex = 8;
             this.label3.Text = "Part";
             // 
+            // panelOnLine1
+            // 
+            this.panelOnLine1.AutoScroll = true;
+            this.panelOnLine1.Location = new System.Drawing.Point(56, 92);
+            this.panelOnLine1.Name = "panelOnLine1";
+            this.panelOnLine1.Size = new System.Drawing.Size(231, 137);
+            this.panelOnLine1.TabIndex = 10;
+            // 
+            // panelOffLine1
+            // 
+            this.panelOffLine1.AutoScroll = true;
+            this.panelOffLine1.Location = new System.Drawing.Point(56, 247);
+            this.panelOffLine1.Name = "panelOffLine1";
+            this.panelOffLine1.Size = new System.Drawing.Size(231, 233);
+            this.panelOffLine1.TabIndex = 11;
+            // 
             // LineUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelOffLine1);
+            this.Controls.Add(this.panelOnLine1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.line1Part);
             this.Controls.Add(this.line3Label);
             this.Controls.Add(this.line3ListBox);
             this.Controls.Add(this.line2Label);
             this.Controls.Add(this.line2ListBox);
             this.Controls.Add(this.line1Label);
-            this.Controls.Add(this.line1ListBox);
             this.Name = "LineUserControl";
             this.Size = new System.Drawing.Size(834, 528);
             this.ResumeLayout(false);
@@ -173,17 +155,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox line1ListBox;
         private System.Windows.Forms.Label line1Label;
         private System.Windows.Forms.Label line2Label;
         private System.Windows.Forms.ListBox line2ListBox;
         private System.Windows.Forms.Label line3Label;
         private System.Windows.Forms.ListBox line3ListBox;
-        private System.Windows.Forms.Label line1Part;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelOnLine1;
+        private System.Windows.Forms.Panel panelOffLine1;
     }
 }
 
