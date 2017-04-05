b9de7bd2287cb447e126c6df590ab98a60963ef0
Bryce Charydczak <bryceromancharydczak@gmail.com>
2017-04-04 23:14:59 -0400

    Add files via upload

---
+++ Bosch_Changeover_App/Bosch_Changeover_App/InformationInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public interface AbstractInformation
+{
+    public AbstractInformation()
+    {
+    }
+        protected Queue<Cards> queue;
+        protected String directoryLocation;
+	
+    abstract int calcTimeToFinish(String x)
+    {
+        //utilizes the cycle time to approximate time before the last of the part
+        //type exits the line
+    }
+
+    abstract int calcTimeRemaining(String x)
+    {
+        //utilizes the cycle time to approximate time before the part type
+        //enters the line
+    }
+
+    abstract int calcPartsRemaining(String x)
+    {
+        //takes part information from ehijunka completed (total counter) and
+        //subtracts from the Access database total parts approximation
+    }
+
+    abstract String calcRealWorldApproximation(int time)
+    {
+        //takes the estimated time variable (Remaining or Finish) and adds it 
+        //to the current real world time to approximate the goal time
+    } 
+
+    abstract String calcNumberofSpanningStations()
+    {
+        //checks for how far the part number spans the designated number 
+        //of stations in a given line
+    }
+    }
