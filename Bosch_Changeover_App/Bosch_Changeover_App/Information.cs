e7544c1a7a0430320cd65854dcf98d1c57f9611d
Eric Hofesmann <hofesmannen@g.cofc.edu>
2017-04-18 18:41:06 -0400

    Add Remove Update Cards

--- Bosch_Changeover_App/Bosch_Changeover_App/Information.cs
+++ Bosch_Changeover_App/Bosch_Changeover_App/Information.cs
@@ -73,10 +73,10 @@ namespace Bosch_Changeover_App
 
             }
             //read information from files
-            while ()
-            {
+//            while ()
+//            {
 
-            }
+//            }
 
             //update array lists
             //send updated information to form1
@@ -191,7 +191,7 @@ namespace Bosch_Changeover_App
 
         }
 
-    */
+    
         public Card getCard(String partType, int line)
         {
             if (line == 1)
